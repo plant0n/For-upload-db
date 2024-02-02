a09f6962f25f9d852c82d5d47f64f543e7ffe63d
msantana <45737195+plant0n@users.noreply.github.com>
2024-02-01 21:04:05 -0400

    This scrip is for upload file .csv

---
+++ uploadCDR2.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#mysqlCommand=$(command -v mysql)
+for FILE in  *.csv;	
+do 
+	echo "Processing file $FILE"	
+	mysql -u root -e "LOAD DATA INFILE '$FILE' IGNORE INTO TABLE database.table FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n' IGNORE 1 ROWS; " 
+    mysql -u root -e "delete from database.table  where `name column` not REGEXP '^[0-9]+$'; "	 
+	mysql -u root -e "update database.table set Report_Date =STR_TO_DATE(SUBSTRING_INDEX(`Start Time GMT`,' ',1), '%m/%d/%Y')  where  report_date is null ; "	
+
+done
+echo "Done"
